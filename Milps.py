17681356fa76604a3235a18e2e3611cd6175f358
Nozidoali <nozidoali@gmail.com>
2023-02-07 00:32:56 +0800

    works on ctrl.aig, level=2

--- Milps.py
+++ Milps.py
@@ -7,16 +7,32 @@ class cut_enumeration_params:
     priority_cut_size: int = 100
     lut_size_limit: int = 6
 
+class Cut:
+    def __init__(self, leaves) -> None:
+        self.leaves: set = set(leaves)
+
+    def __str__(self) -> str:
+        leaves = list(self.leaves)
+        leaves.sort() # alphabetic order
+        return ','.join(leaves)
+
+    def __hash__(self) -> int:
+        return hash(self.__str__)
+    
+    def __add__(self, other: 'Cut') -> 'Cut':
+        return Cut(self.leaves.union(other.leaves))
+    
+    def size(self) -> int:
+        return len(self.leaves)
+
 def cut_enumeration(g: BLIFGraph) -> dict:
     cuts: dict = {}
     for n in g.topological_traversal():
-        if n in g.nodes and len(g.node_fanins[n]) >= 1:
+        cuts[n] = [Cut([n])]
+        if n in g.node_fanins:
             c = [cuts[f] for f in g.node_fanins[n]]
-            cutset = merge_cuts(c, cut_enumeration_params.priority_cut_size)
-            cuts[n] = [list(g.node_fanins[n])] + cutset[:]  # slicing triggers deep copy
-        else:
-            cuts[n] = [[n]]  # unit cut
-    return cuts
+            cuts[n] += merge_cuts(c, cut_enumeration_params.priority_cut_size)[:]
+    return cuts # uniqify
 
 
 def merge_cuts(cuts: list, setsize: int):
@@ -24,15 +40,18 @@ def merge_cuts(cuts: list, setsize: int):
         return []
     if len(cuts) == 1:
         return cuts[0]
-    cutset = []
-    remains = merge_cuts(cuts[1:], setsize)
-    for remain in remains:
-        for cut in cuts[0]:
-            c = list(set(cut).union(set(remain)))
-            if len(c) <= cut_enumeration_params.lut_size_limit:
-                cutset.append(c)
-    random.shuffle(cutset)
-    return cutset[:setsize]
+    
+    # set of cut
+    cutset = set()
+
+    remains: list = merge_cuts(cuts[1:], setsize)
+    for cut in cuts[0]:
+        for remain in remains:
+            c: Cut = cut + remain
+            if c.size() <= cut_enumeration_params.lut_size_limit:
+                cutset.add(c)
+
+    return list(cutset)
 
 
 def small_blif() -> BLIFGraph:
@@ -42,23 +61,26 @@ def small_blif() -> BLIFGraph:
      \  /
       n3  n4
        \  /
-        n5
+        n5  n6
+         \  /
+          n7  n8
+           \ /
+            n9
     """
     g: BLIFGraph = BLIFGraph()
 
-    g.inputs.add('n1')
-    g.inputs.add('n2')
-    g.inputs.add('n4')
+    g.create_pi('n1')
+    g.create_pi('n2')
+    g.create_pi('n4')
+    g.create_pi('n6')
+    g.create_pi('n8')
 
-    g.outputs.add('n5')
+    g.create_po('n9')
 
-    g.nodes.add('n3')
-    g.node_fanins['n3'] = set(['n1', 'n2'])
-    g.node_funcs['n3'] = ['11 1']
-
-    g.nodes.add('n5')
-    g.node_fanins['n5'] = set(['n3', 'n4'])
-    g.node_funcs['n5'] = ['11 1']
+    g.create_and('n1', 'n2', 'n3')
+    g.create_and('n3', 'n4', 'n5')
+    g.create_and('n5', 'n6', 'n7')
+    g.create_and('n7', 'n8', 'n9')
 
     g.traverse()
 
@@ -100,7 +122,7 @@ def export_milps(g: BLIFGraph, clock_period: int):
         # clock period
         for n in g.nodes:
             i = signal_to_var[n]
-            m.addConstr(i <= cp, f"cp_{i}")
+            m.addConstr(i <= cp, f"cp_{n}")
 
         # cut selection
         cuts = cut_enumeration(g)
@@ -123,24 +145,19 @@ def export_milps(g: BLIFGraph, clock_period: int):
 
                 # delay propagation if the cut is chosen
                 d_i = signal_to_var[n]
-                for f in cut_set[c]:
+                for f in cut_set[c].leaves:
                     d_j = signal_to_var[f]
-                    m.addConstr(d_i + (1-y) * milp_params.infinity >= d_j + 1, f"in_{n}")
+                    m.addConstr(d_i + (1-y) * milp_params.infinity >= d_j + 1)
 
             # at least one cut need to be chosen
             # reference: https://www.gurobi.com/documentation/10.0/refman/py_model_addconstrs.html
             m.addConstr(sum(cut_selection_vars) >= 1)
 
-
-        for n in g.nodes:
-            i = signal_to_var
-            cuts_i = cuts[n]
-            print(cuts_i)
-
         # Optimize model
-        m.optimize()
-
         m.write('test.lp')
+        m.optimize()
+        m.write('test.sol')
+
 
         for v in m.getVars():
             print('%s %g' % (v.VarName, v.X))
@@ -158,6 +175,6 @@ def export_milps(g: BLIFGraph, clock_period: int):
 
 
 if __name__ == "__main__":
-    g = small_blif()
+    g = read_graph_from_blif('max.blif')
 
     export_milps(g, clock_period= 3)
