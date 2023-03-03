841c472d4940a7080a0a4c7ce93e6876670af44e
nozidoali <nozidoali@gmail.com>
2023-03-03 16:37:26 +0100

    add install script

---
+++ install_apt_packages.sh
@@ -0,0 +1,13 @@
+
+sudo apt-get update
+
+sudo apt-get install -y \
+    make \
+    cmake \
+    build-essential \
+    pkg-config \
+    python3-dev \
+    python3-venv
+
+sudo apt-get install -y \
+    graphviz
