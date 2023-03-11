e38faafdc3ca7cb81d689ea1b95ad351ad24682f
nozidoali <nozidoali@gmail.com>
2023-03-12 00:59:06 +0100

    fix the directories

--- RegressionTest/TestCases/Generators/DFG.py
+++ RegressionTest/TestCases/Generators/DFG.py
@@ -2,4 +2,4 @@ from TestCases.Generators.FPL import *
 
 def fpl_dfg_files():
     for example in all_examples():
-        yield f"Examples/{example}/{example}.dot"
+        yield f"Examples/{example}/reports/{example}.dot"
--- RegressionTest/TestCases/Generators/Verilog.py
+++ RegressionTest/TestCases/Generators/Verilog.py
@@ -2,4 +2,4 @@ from TestCases.Generators.FPL import *
 
 def fpl_verilog_files():
     for example in all_examples():
-        yield f"Examples/{example}/{example}.v"
+        yield f"Examples/{example}/reports/{example}.v"
