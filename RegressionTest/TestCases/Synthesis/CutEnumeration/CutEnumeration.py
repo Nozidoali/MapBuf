c6799cf7a16d33e4299a38c99d81e8eb9caf8cb0
nozidoali <nozidoali@gmail.com>
2023-03-14 21:51:06 +0100

    cutless enumeration

--- RegressionTest/TestCases/Synthesis/CutEnumeration/CutEnumeration.py
+++ RegressionTest/TestCases/Synthesis/CutEnumeration/CutEnumeration.py
@@ -5,22 +5,31 @@
 Author: Hanyu Wang
 Created time: 2023-03-11 20:26:33
 Last Modified by: Hanyu Wang
-Last Modified time: 2023-03-14 21:40:49
+Last Modified time: 2023-03-14 21:50:53
 '''
 
 from MADBuf import *
 from TestCases.TestCases import *
 from TestCases.Generators import *
 
-def test_all_benchmarks() -> None:
+def test_cut_enumeration_on_all_benchmarks() -> None:
     for file in dac_blif_files():
         print(f"Testing cut enumeration on {file}...")
         blif = read_blif(file)
-        signal_to_cuts = cut_enumeration(blif, num_cuts=2, cut_size=3)
+        signal_to_cuts = cut_enumeration(blif, num_cuts=4, cut_size=3)
 
         # check if the cut enumeration is correct
-        check_cuts(blif, signal_to_cuts, num_cuts=2, cut_size=3, verbose=True)
+        check_cuts(blif, signal_to_cuts, num_cuts=4, cut_size=3, verbose=True)
 
+def test_cutless_enumeration_on_all_benchmarks() -> None:
+    for file in dac_blif_files():
+        print(f"Testing cut enumeration on {file}...")
+        blif = read_blif(file)
+        signal_to_cuts = cut_enumeration(blif, num_cuts=4, cut_size=3, cutless=True)
+
+        # check if the cut enumeration is correct
+        check_cuts(blif, signal_to_cuts, num_cuts=4, cut_size=3, verbose=True)
+        
 class TestCutEnumeration(TestCases):
 
     def test(self) -> None:
