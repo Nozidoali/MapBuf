7510c54583c3704700d6538f5ffbbff15a8a23ae
hanyu <nozidoali@gmail.com>
2025-03-10 11:10:21 -0700

    bugfix: dependencies version

--- setup.py
+++ setup.py
@@ -4,5 +4,5 @@ setup(
     name="mapbuf",
     version="0.1.0",
     packages=find_packages(include=["MADBuf", "MADBuf.*"]),
-    install_requires=["gurobipy==10.0.1", "pygraphviz==1.10"],
+    install_requires=["gurobipy>=10.0.1", "pygraphviz>=1.10"],
 )
